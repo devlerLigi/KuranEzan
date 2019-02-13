366df0689d2419753289c8931951406183496b0b
ASUS <topaloglun@itu.edu.tr>
2019-02-13 05:32:15 +0300

    namaz vakitleri tabı düzenlemeler..

--- app/src/main/java/com/uren/kuranezan/MainFragments/TabImsakiye/ImsakiyeFragment.java
+++ app/src/main/java/com/uren/kuranezan/MainFragments/TabImsakiye/ImsakiyeFragment.java
@@ -1,21 +1,43 @@
 package com.uren.kuranezan.MainFragments.TabImsakiye;
 
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
 import com.uren.kuranezan.MainFragments.BaseFragment;
 import com.uren.kuranezan.R;
 
+import java.util.Locale;
+
+import butterknife.BindDimen;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
 public class ImsakiyeFragment extends BaseFragment {
 
-    @BindView(R.id.txtToolbarTitle)
-    TextView txtToolbarTitle;
+    View mView;
+    //@BindView(R.id.txtToolbarTitle)
+    //TextView txtToolbarTitle;
+    @BindView(R.id.imgBackground)
+    ImageView imgBackground;
+    @BindView(R.id.imgDarkGradient)
+    ImageView imgDarkGradient;
+
+    @BindView(R.id.txtRemaining)
+    TextView txtRemaining;
+
+    private CountDownTimer mCountDownTimer;
+    private boolean mTimerRunning;
+    private long mTimeLeftInMillis = START_TIME_IN_MILLIS;
+    private static final long START_TIME_IN_MILLIS = 60000;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,13 +48,19 @@ public class ImsakiyeFragment extends BaseFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_main_imsakiye, container, false);
-        ButterKnife.bind(this, view);
+        if(mView == null){
+            mView = inflater.inflate(R.layout.fragment_main_imsakiye, container, false);
+            ButterKnife.bind(this, mView);
 
-        setToolbar();
+            setToolbar();
 
-        return view;
+            setUI();
+            startTimer();
+        }
+
+        return mView;
     }
+
 
     @Override
     public void onStart() {
@@ -41,7 +69,56 @@ public class ImsakiyeFragment extends BaseFragment {
     }
 
     private void setToolbar() {
-        txtToolbarTitle.setText(getString(R.string.prayer));
+        //txtToolbarTitle.setText(getString(R.string.prayer));
+    }
+
+    private void setUI() {
+
+        Glide.with(getContext())
+                .load(R.drawable.wallpaper7)
+                .apply(RequestOptions.centerCropTransform())
+                .into(imgBackground);
+        Glide.with(getContext())
+                .load(R.drawable.gradient_dark)
+                .apply(RequestOptions.centerCropTransform())
+                .into(imgDarkGradient);
+
+
+    }
+
+    private void startTimer() {
+
+        mCountDownTimer = new CountDownTimer(mTimeLeftInMillis, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                mTimeLeftInMillis = millisUntilFinished;
+                updateCountDownText();
+            }
+
+
+            @Override
+            public void onFinish() {
+                mTimerRunning = false;
+                mTimeLeftInMillis = START_TIME_IN_MILLIS;
+                startTimer();
+                Log.i("info", "Timer start again");
+            }
+        }.start();
+
+        mTimerRunning = true;
+
+    }
+
+    private void updateCountDownText() {
+
+        int minutes = (int) (mTimeLeftInMillis / 1000) / 60;
+        int seconds = (int) (mTimeLeftInMillis / 1000) % 60;
+
+        String timeLeftFormatted = String.format(Locale.getDefault(), "%02d:%02d", minutes, seconds);
+
+        txtRemaining.setText(timeLeftFormatted);
+
+
     }
 
 
